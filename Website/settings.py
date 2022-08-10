472cf9e1f685a8be59440e865a0e578757b19ce7
MohanadOmarrr <102043745+MohanadOmarrr@users.noreply.github.com>
2022-08-10 15:18:37 +0200

    var files

--- Website/settings.py
+++ Website/settings.py
@@ -133,8 +133,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
